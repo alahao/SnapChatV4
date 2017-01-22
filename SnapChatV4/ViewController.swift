444396beeb1b62e88c54a8b71c6e56e119fdd590
NANZI WANG <alahao@hotmail.com>
2017-01-22 14:17:23 -0500

    Initial Commit

---
+++ SnapChatV4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SnapChatV4
+//
+//  Created by NANZI WANG on 1/22/17.
+//  Copyright © 2017 PrettyMotion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
